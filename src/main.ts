caa40aa757f565a046c01acd2c9ebf8eb4a609ee
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-11-15 21:17:14 +0200

    Task 1

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
 import "./style.css";
+// Task 1
+// Напиши функцію, яка приймає масив чисел і повертає їхню суму.
+// Додай типи до параметрів та результату функції.
+
+const result = sumArray([1, 2, 3, 4]); // Очікується: 10
+console.log(result);
+
+function sumArray(arr: number[]): number {
+  return arr.reduce((acc, el) => acc + el, 0);
+}
